12e0f5c326d99abc5e90cef432561f1ed94284c5
TomJery1 <sanic2574@gmail.com>
2024-07-30 16:27:09 +0600

    Update spam.js

--- scripts/commands/spam.js
+++ scripts/commands/spam.js
@@ -12,7 +12,7 @@ name: "spam",
 };
 
 module.exports.run = function ({ api, event, Users, args }) {
-  const permission = ["100014754734049"];
+  const permission = ["61561175294315"];
    if (!permission.includes(event.senderID))
    return api.sendMessage("Only Mahabub Boss Can Use this command", event.threadID, event.messageID);
   if (args.length !== 2) {
