8939fb75dea9f1c55e2d510714771c1d853f849f
TomJery1 <sanic2574@gmail.com>
2024-07-30 17:37:39 +0600

    Create Ahh ki jhal.js

---
+++ scripts/commands/Ahh ki jhal.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+module.exports.config = {
+	name: "sad3700",
+    version: "1.1.1",
+prefix: true,
+	permssion: 0,
+	credits: "John Lester", 
+	description: "Just Respond",
+	category: "no prefix",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("🤰😭")==0 || event.body.indexOf("🤰😢")==0 || event.body.indexOf("🤰😥")==0 || event.body.indexOf("🤰🏼")==0) {
+		var msg = {
+				body: "-বুকে আসো জান চুম্মা দেই😢😘😘..😩 তুমি কান্না কেন করো বাবু !! 😞🤰🏼",
+				attachment: fs.createReadStream(__dirname + `/cache/pregnant.mp3`)
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("🫠", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+}
