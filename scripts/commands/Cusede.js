a1813c52305b06e74f12567c43a62b176475d2c8
Tombot444 <djpriya325@gmail.com>
2024-08-01 14:00:17 +0600

    Create Cusede.js

---
+++ scripts/commands/Cusede.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+module.exports.config = {
+	name: "valobasa",
+    version: "1.1.1",
+prefix: true,
+	permssion: 0,
+	credits: "John Lester", 
+	description: "Just Respond",
+	category: "no prefix",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	let react = event.body.toLowerCase();
+	if(react.includes("cusede") || react.includes("cus") || react.includes("cus")) {
+		var msg = {
+				body: "পুরাটা মুখে নিছি🥵🥵🥵"
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+	   }
