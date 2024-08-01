7a7cc0bacba9b5f9ea4508e7d509f7cf6675464a
Tombot444 <djpriya325@gmail.com>
2024-08-01 13:33:54 +0600

    Create Valobasa.js

---
+++ scripts/commands/Valobasa.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+module.exports.config = {
+	name: "valobasa",
+    version: "1.1.1",
+prefix: true,
+	permssion: 0,
+	credits: "John Lester", 
+	description: "Just Respond",
+	category: "no prefix",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	let react = event.body.toLowerCase();
+	if(react.includes("Sali") || react.includes("magi") || react.includes("cudi")) {
+		var msg = {
+				body: "চুপ কর সালা/সালি😡😡"
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+	   }
